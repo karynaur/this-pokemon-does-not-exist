0c7b5e600193758daa4816d8473a97e9bf2bccb2
unknown <vishallokare11502@gmail.com>
2021-04-04 16:48:22 +0530

    changed scroll final

--- script.js
+++ script.js
@@ -5,35 +5,29 @@ function loadimgs() {
     //document.getElementById('0').src = '0.png';
 
     // for(int in=0; in<10; in++){
-        
     //     document.getElementById(in+'').src = in+'.png';
     // }
 
 
-    var i = 0;
-    while(i <= 6){
-        var ImageSrc = i + ".png";
-        console.log(ImageSrc);
-        document.getElementById('0').src = ImageSrc;
-        i++;
-    }
+    //var i = 0;
+    //while(i <= 6){
+    //    var ImageSrc = i + ".png";
+    //    console.log(ImageSrc);
+    //    document.getElementById('0').src = ImageSrc;
+    //    i++;
+    //}
 
 
 
     rightdown();
-
-    
 }
-
-
 
 var a=0;
 
 function rightdown() {
     let c = document.querySelector('#cont');
     c.scrollLeft += 2;
-    window.scrollBy(0, 1); 
-
+    window.scrollBy(0, 1);
     a++;
     if(a<700){
         scrolldelay = setTimeout('rightdown()', 10); // scrolls every 10 milliseconds
@@ -41,12 +35,10 @@ function rightdown() {
     else{
         a=0;
         leftdown();
-    }   
-    
+    }
 }
 
 function leftdown(){
-    
     // alert('ini');
     let c = document.querySelector('#cont');
     c.scrollLeft -= 2;
@@ -59,7 +51,6 @@ function leftdown(){
         a=0;
         //location.reload();
         rightup();
-        
     }
 }
 
